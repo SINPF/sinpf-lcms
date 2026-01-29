808bfaf0be8aa55ac9e7f7bebee798afdad55b36
b-tupiti <tupitibrandon@gmail.com>
2026-01-30 09:14:56 +1100

    Remove bottom margin

--- app/(dashboard)/components/header.tsx
+++ app/(dashboard)/components/header.tsx
@@ -2,7 +2,7 @@
 function Header() {
  
     return ( 
-        <header className="bg-white border-l-0  flex flex-col md:flex-row md:items-center justify-between gap-6 mb-12 border-b  border-slate-200 p-8">
+        <header className="bg-white border-l-0  flex flex-col md:flex-row md:items-center justify-between gap-6 border-b  border-slate-200 p-8">
             <h1 className="font-bold text-xl text-slate-900 -tracking-normal">
               Welcome back, <span className="text-[#002B5C] ">Brandon</span>
             </h1>
