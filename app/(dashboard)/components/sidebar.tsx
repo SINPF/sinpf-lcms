f59f057c04f173d093f4511e81729e2154ddc37e
b-tupiti <tupitibrandon@gmail.com>
2026-01-29 23:21:54 +1100

    Update file

--- app/(dashboard)/components/sidebar.tsx
+++ app/(dashboard)/components/sidebar.tsx
@@ -5,7 +5,6 @@ import SignOutButton from "@/components/signout-button";
 function Sidebar() {
   return (
     <aside className="w-72 bg-white border-r border-slate-200 flex flex-col lg:flex h-screen sticky top-0">
-      
       <div className="p-8">
         <BrandArea />
         <NavMenu />
