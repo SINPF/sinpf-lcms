c987d58bf90f0f728fccc41b8eedfdbefeda9f8b
b-tupiti <tupitibrandon@gmail.com>
2026-01-30 15:16:51 +1100

    Update logout

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 function SignOutButton() {
     return ( 
-         <Link 
+        <Link 
           href={'/login'}
-          className="flex items-center gap-3 px-4 py-3 rounded-xl text-slate-400 hover:text-rose-600 hover:bg-rose-50 transition-all text-sm font-bold group"
+          className="flex items-center gap-2 px-4 py-3 rounded-xl text-slate-500  text-sm transition-all duration-200  group border border-transparent "
         >
-          <LogOut className="w-4 h-4 group-hover:rotate-180 transition-transform duration-500" />
-          Sign Out
+          <LogOut className="w-4 h-4 " />
+          <span>Logout</span>
         </Link>
-     );
+    );
 }
 
 export default SignOutButton;
