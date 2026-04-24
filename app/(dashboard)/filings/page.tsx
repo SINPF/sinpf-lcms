4a95348c21a329dafda98b25770d6a092361651f
b-tupiti <tupitibrandon@gmail.com>
2026-04-24 11:18:08 +1100

    chore: remove tabs

--- app/(dashboard)/filings/page.tsx
+++
@@ -1,7 +0,0 @@
-function FilingsPage() {
-    return ( 
-        <h1 className="text-xl font-bold text-slate-500">Filings Dashboard</h1>
-     );
-}
-
-export default FilingsPage;
--- app/(dashboard)/reports/page.tsx
+++
@@ -1,7 +0,0 @@
-function ReportsPage() {
-    return ( 
-        <h1 className="text-xl font-bold text-slate-500">Reports Dashboard</h1>
-     );
-}
-
-export default ReportsPage;
